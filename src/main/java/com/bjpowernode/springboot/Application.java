f59bb868611b9a53f0fb07e207e9a0633385f6c6
taotao-coder-code <75291666+taotao-coder-code@users.noreply.github.com>
2021-03-25 23:41:38 +0800

    Initial commit

---
+++ src/main/java/com/bjpowernode/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SpringBoot项目启动入口类
+@SpringBootApplication  //开启springboot配置
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
